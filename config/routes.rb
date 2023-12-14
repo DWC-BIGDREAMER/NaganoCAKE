d63c42d92f5c8c3ef2fe8a0fa4d98206ea020dba
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-12-14 07:46:40 +0000

    [Add]customersのroutes記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  #rupblicは表示させない
-  #homes
+  #publicは表示させない
   scope module: :public do
+
+  #homes
   root to: "homes#top"
-  get "/about" => "homes#about", as:"about"
+  get "about" => "homes#about", as:"about"
+
+  #cutomers
+  get "customers/my_page" => "customers#show"
+  get "customers/information/edit" => "customers#edit"
+  patch "customers/information" => "customers#update"
+  get "customers/unsubscribe" => "customers#unsubscribe"
+  patch "customers/withdraw" => "customers#withdraw"
   end
 
 end
