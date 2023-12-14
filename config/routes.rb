13a0e1451d0ac5b321d6bb9d1a21eaf7d050cafd
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-12-14 07:08:44 +0000

    Add pub/homes routes top,about

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #rupblicは表示させない
+  #homes
+  scope module: :public do
+  root to: "homes#top"
+  get "/about" => "homes#about", as:"about"
+  end
+
 end
