062f41e08560c1d36139b177ba37e49ea06bcafa
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-12-14 08:03:57 +0000

    [add]customersテーブルのis_activeカラムにデフォルト値「true」追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_14_072332) do
+ActiveRecord::Schema.define(version: 2023_12_14_080038) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2023_12_14_072332) do
     t.string "postcode", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
-    t.boolean "is_active", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
