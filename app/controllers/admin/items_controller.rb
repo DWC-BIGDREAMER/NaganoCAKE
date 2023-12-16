cfdd8fa411e30f6c4f0c745fda32d9315ed7dd1b
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-12-16 11:11:49 +0000

    [Add＆Fix]itemコントローラー、admin新規商品追加にジャンル選択プルダウン

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,18 +5,45 @@ class Admin::ItemsController < ApplicationController
   end
 
   def index
+    @items = Item.page(params[:id])
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render "new"
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render "show"
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id,
+                                 :name,
+                                 :introduction,
+                                 :price,
+                                 :is_active,
+                                 :image
+                                 )
   end
 
 end
