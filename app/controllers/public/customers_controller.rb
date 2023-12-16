b023d03db9e23dc16484796adbf338dcc7a9649b
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-12-16 07:33:55 +0000

    [Add]マイページ/customer実装

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,31 @@
 class Public::CustomersController < ApplicationController
+
+  def show
+    @cus = current_customer
+  end
+
+  def edit
+    @cus = current_customer
+  end
+
+  def update
+    customer = current_customer
+    customer.update(customer_params)
+    redirect_to customers_my_page_path
+  end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:family_name,
+                                     :first_name,
+                                     :family_name_kana,
+                                     :first_name_kana,
+                                     :postcode,
+                                     :address,
+                                     :telephone_number,
+                                     :email,
+                                    )
+  end
+
 end
