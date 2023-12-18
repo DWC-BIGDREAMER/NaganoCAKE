2e13bd8c6b12f153a129fa4b127fa271154de98e
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-12-18 06:19:38 +0000

    [add]customer商品一覧ページ作成（ジャンル窓なし）

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,9 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:page]).per(8).order('created_at DESC')
+    @items_count = Item.count
+    @genres = Genre.all
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -19,10 +19,19 @@ class Item < ApplicationRecord
       "販売停止中"
     end
   end
-  
+
   #税込価格
   def taxed_price
     (self.price * 1.10).round
   end
 
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join("app/assets/images/sato.cake/皿にケーキ.jpg")
+      image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/*")
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
+
