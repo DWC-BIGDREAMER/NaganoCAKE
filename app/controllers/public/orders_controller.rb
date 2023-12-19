0411f5f53bde8262367dbf412cb5b929955a9a89
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-12-19 06:51:37 +0000

    [fix]注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -15,9 +15,36 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
-  end
+  end 
 
   def confirm
+    cc = current_customer
+    # 自身の住所/登録済み住所/新しい住所のどれが選ばれたかを確認。
+    which_address = params[:order][:which_address]
+    # 登録済み住所が選ばれた場合、そのidからデータを取得
+    chosen_address = Address.find(params[:order][:address_id])
+    
+    order = Order.new(params_order)
+    order.customer_id = cc.id
+    order.shipping_fee = 800
+    order.status = 0
+    order.total_payment = cc.cart_items.sum(&:sum)
+    
+    case which_address.to_i
+    # 自身の住所
+    when 0 then
+      order.name = cc.full_name
+      order.postcode = cc.postcode
+      order.address = cc.address
+    # 登録済み住所
+    when 1 then
+      order.name = chosen_address.name
+      order.postcode = chosen_address.postcode
+      order.address = chosen_address.address
+    end
+    # 新しい住所を選んだ場合、name, postcode, addressは送られてくるので何か処理する必要なし。
+    
+      
   end
 
   def compretion
