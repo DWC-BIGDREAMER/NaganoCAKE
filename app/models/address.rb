2f31417dafac3b75f8de15bb67bb75aa3e4b6206
EOwRHgjWLA <148323469+EOwRHgjWLA@users.noreply.github.com>
2023-12-19 10:15:09 +0000

    [Add]配送先登録/一覧ページ

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
   belongs_to :customer
+
+  validates :name, :address, presence: true
+  validates :address, length: { maximum: 50 }
+  validates :postcode, format: {with: /\A\d{7}\z/}, presence: true
+
+  def mark_postcode
+    "〒" + postcode
+  end
+
+
 end
