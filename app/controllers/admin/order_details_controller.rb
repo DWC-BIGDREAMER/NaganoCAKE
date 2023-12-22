865bdf9d4a4dd58bf90d3c3ae37caff8367f304f
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-12-22 05:18:36 +0000

    [update]管理者注文詳細画面、statusの変更連動

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,10 +1,16 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
     ol = OrderDetail.find(params[:order_detail][:order_detail_id])
+    od = Order.find(params[:id])
     ol.update(params_order_detail)
-    redirect_to admin_order_path(ol.order)
-    @od = Order.find(params[:id])
-    @ols = @od.order_details
+    # byebug
+    if od.order_details.all? {|order_detail| order_detail.making_status == "making_completed"}
+      od.update(status: :preparing)
+      redirect_to admin_order_path(ol.order)
+      @od = Order.find(params[:id])
+      @ols = @od.order_details
+    end 
+    
   end 
   
   private
